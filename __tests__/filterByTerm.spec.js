ba991bd7f69e205eeff456eea0f5cbb8a021cf5e
c.p.kondas <c.p.kondas@leidos.com>
2020-07-08 07:36:55 -0400

    Fixed typo and added a few comments

--- __tests__/filterByTerm.spec.js
+++ __tests__/filterByTerm.spec.js
@@ -10,11 +10,12 @@ describe("Filter function", () => {
 
         const output = [{ id: 3, url: "https://www.link3.dev" }];
 
+        // Check that the filtered input matches the output exactly
         expect(filterByTerm(input, "link")).toEqual(output);
         expect(filterByTerm(input, "LINK")).toEqual(output);
 
     }),
-    test("it should filter by a search term (uri)", () => {
+    test("it should filter by a search term (url)", () => {
         const input = [
             { id: 1, url: "https://www.url1.dev" },
             { id: 2, url: "https://www.url2.dev" },
@@ -23,6 +24,7 @@ describe("Filter function", () => {
 
         const output = [{ id: 3, url: "https://www.url3.dev" }];
 
+        // An array will be returned, so check that the output is contained in the array
         expect(filterByTerm(input, "url")).toEqual(expect.arrayContaining(output));
         expect(filterByTerm(input, "URL")).toEqual(expect.arrayContaining(output));
 
